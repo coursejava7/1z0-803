1483fb6820d9a9b68cef19b623b29fec64c3ad3d
blesa <blesa@ub.edu>
2016-06-01 16:54:04 +0200

    Added ReturnClass to demonstrate return value concepts

---
+++ src/main/java/practices/methods/returned/values/ReturnClass.java
@@ -0,0 +1,32 @@
+package practices.methods.returned.values;
+
+class BaseClass {
+	
+	public void doSomething() {
+		System.out.println("Hello from BaseClass.doSomething method");
+	}
+}
+
+class ChildClass extends BaseClass{
+	
+	public void doSomething() {
+		System.out.println("Hello from ChildClass.doSomething method");
+	}
+}
+
+public class ReturnClass extends BaseClass{
+	
+	public BaseClass miMethod(BaseClass clazz){
+		clazz.doSomething();
+		return this;
+	}
+	
+	public static void main(String[] args){
+		
+		ReturnClass rc = new ReturnClass();
+		BaseClass bc = new BaseClass();
+		rc.miMethod(rc);
+		rc.miMethod(bc);
+		
+	}
+}
