74cc76a1d3173e43cffeed193df5a2c5f658a581
coursejava7 <coursejava7@gmail.com>
2016-06-22 16:42:01 +0200

    Added some comments to t-w-r class

--- src/main/java/practices/exceptions/TryWithResources.java
+++ src/main/java/practices/exceptions/TryWithResources.java
@@ -5,10 +5,25 @@ import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * This class tries to demonstrate how works try-with-resources block construction
+ * Two methods are included one with a BufferedREader and other with a Autoclosable
+ * object wich tries to demonstrate the order of execution when an Exception is
+ * thrown.
+ * 
+ *  The order is:
+ *  
+ *     - closing resources declared in the try-with-resources block.
+ *     - catch block
+ *     - finally block
+ *  
+ * @author blesa
+ *
+ */
 public class TryWithResources {
 	
 	public void printFile() {
-
+		
 		try(BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("./trywithresources.txt")))){
 			String line;
 			while((line = bufferedReader.readLine()) != null){
@@ -23,18 +38,24 @@ public class TryWithResources {
 	}
 	
 	/**
+	 * This method uses an autoclosable object as a wrapper of a 
+	 * BufferedReader
 	 * 
 	 * @param file absolute path pointing at the file we need to read
+	 * @throws IOException 
 	 */
 	public void printFile(String file) throws IOException {
 
-		try(BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(file)))){
+		try(MyClosableClass myCC = new MyClosableClass(file)){
 			String line;
-			while((line = bufferedReader.readLine()) != null){
+			while((line = myCC.readLine()) != null){
 				System.out.println(line);
 			}
+		}catch (IOException ioe){
+			System.out.println("Executing catch block");
+			throw ioe;
 		}finally{
-			System.out.println("Executing finally code block");
+			System.out.println("Executing finally block");
 		}
 	}
 	
@@ -42,8 +63,32 @@ public class TryWithResources {
 	public static void main(String[] args){
 		
 		TryWithResources twr = new TryWithResources();
-		twr.printFile();
+		try {
+		twr.printFile("./trywithresources.txt");
+		} catch (IOException ioe){
+			System.out.println("handling the propagated IOException from main method");
+		}
+	}
+}
+	
+class MyClosableClass implements AutoCloseable {
+	
+	private final BufferedReader br;
+	
+	public MyClosableClass(String file) throws IOException {
+		this.br = new BufferedReader(new FileReader(new File(file)));
 	}
 	
+	public String readLine() throws IOException{
+		
+		if(true)
+			throw new IOException("Houston!!! We have a problem!!!");
+		return br.readLine();
+	}
 	
+	@Override
+	public void close() throws IOException {
+		System.out.println("closing buffer reader...");
+		br.close();
+	}
 }
