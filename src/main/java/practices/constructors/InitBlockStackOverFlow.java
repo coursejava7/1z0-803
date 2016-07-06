1b34f4a71c386d241440bdadc13a2e313db3ac28
coursejava7 <coursejava7@gmail.com>
2016-07-06 09:10:34 +0200

    Added a class to demonstrate how works the init block and how we can get a StackOverFlowError

---
+++ src/main/java/practices/constructors/InitBlockStackOverFlow.java
@@ -0,0 +1,33 @@
+package practices.constructors;
+
+public class InitBlockStackOverFlow {
+	
+	{
+		doSomething(10);// no problem we can invoke instance methods from init blocks
+		doSomething(); // no problem yet. Static methods can be invoked due that the static context was loaded before.
+		
+		/* Runtime exception.... Before the constructor the init block is executed... 
+		   And the init block invokes the constructor again... So this call loop will exhaust the call stack memory.
+		*/ 
+		new InitBlockStackOverFlow(); 
+	}
+	
+	InitBlockStackOverFlow(){
+		System.out.println("constructing with non-parameters constructor...");
+	}
+	
+	public void doSomething(){
+		System.out.println("doing something...");
+	}
+	
+	public static void doSomething(int x){
+		System.out.println("doing something statically...");
+	}
+	
+	public static void main(String[] args){
+		
+		InitBlockStackOverFlow ib = new InitBlockStackOverFlow();
+		
+	}
+
+}
